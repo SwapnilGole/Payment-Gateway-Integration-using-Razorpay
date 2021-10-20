87c69404d40d1bc915f8bb653d90a4d515076c22
Swapnil Gole <swapnilgole2000@gmail.com>
2021-10-20 20:36:16 +0530

    api key changes

--- js/app.js
+++ js/app.js
@@ -14,32 +14,32 @@
                 console.log(userName)
                 console.log(userAmount)
                 var options = {
-            "key": "rzp_test_IgZLQQBRAjqrlI", // Enter the Key ID generated from the Dashboard
-            "amount":userAmount*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-            "currency": "INR",
-            "name": userName,
-            "description": "The Sparks Foundation",
-            "image": "https://i.ibb.co/nCXVp2D/sparkslogo.png",
-            // "order_id": "order_Ef80WJDPBmAeNt", //Pass the `id` obtained in the previous step
-            // "account_id": "acc_Ef7ArAsdU5t0XL",
-            "handler": function (response){
-                // alert(response.razorpay_payment_id);
-                // alert(response.razorpay_order_id);
-                // alert(response.razorpay_signature)
-                jQuery.ajax({
-                    // type:"POST",
-                    // url:"payment.php",
-                    // data:"pay_id="+response.razorpay_payment_id+"&amount="+amount+"&name="+name,
-                    success:function(result){
-                        window.location.href="success.html"
-                    },
-                    failure:function(result){
-                        window.location.href="index.html"
-                    }
-                    
-                })
-            }
-        };
+                  // "key": "rzp_test_IgZLQQBRAjqrlI", // Enter the Key ID generated from the Dashboard
+                  key: "rzp_live_SmwguoXr3VsU84", // Enter the Key ID generated from the Dashboard
+                  amount: userAmount * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+                  currency: "INR",
+                  name: userName,
+                  description: "The Sparks Foundation",
+                  image: "https://i.ibb.co/nCXVp2D/sparkslogo.png",
+                  // "order_id": "order_Ef80WJDPBmAeNt", //Pass the `id` obtained in the previous step
+                  // "account_id": "acc_Ef7ArAsdU5t0XL",
+                  handler: function (response) {
+                    // alert(response.razorpay_payment_id);
+                    // alert(response.razorpay_order_id);
+                    // alert(response.razorpay_signature)
+                    jQuery.ajax({
+                      // type:"POST",
+                      // url:"payment.php",
+                      // data:"pay_id="+response.razorpay_payment_id+"&amount="+amount+"&name="+name,
+                      success: function (result) {
+                        window.location.href = "success.html";
+                      },
+                      failure: function (result) {
+                        window.location.href = "index.html";
+                      },
+                    });
+                  },
+                };
         var rzp1 = new Razorpay(options);
         document.getElementById('donate-btn').onclick = function(){
             rzp1.open();
